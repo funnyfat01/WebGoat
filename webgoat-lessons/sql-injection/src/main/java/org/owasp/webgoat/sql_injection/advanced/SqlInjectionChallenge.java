1450ce1607fc2a765f8943d1d99f06b0607f92e7
funnyfat01 <91549339+funnyfat01@users.noreply.github.com>
2022-03-31 18:03:03 +0200

    Update SqlInjectionChallenge.java
    fix vulnerability

--- webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/advanced/SqlInjectionChallenge.java
+++ webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/advanced/SqlInjectionChallenge.java
@@ -60,8 +60,9 @@ public class SqlInjectionChallenge extends AssignmentEndpoint {
 
 
             try (Connection connection = dataSource.getConnection()) {
-                String checkUserQuery = "select userid from sql_challenge_users where userid = '" + username_reg + "'";
-                Statement statement = connection.createStatement();
+                String checkUserQuery = "select userid from sql_challenge_users where userid = ?";
+                PreparedStatement statement = connection.createStatement(checkUserQuery);
+                statement.setString(1,username_reg);
                 ResultSet resultSet = statement.executeQuery(checkUserQuery);
 
                 if (resultSet.next()) {
@@ -80,7 +81,7 @@ public class SqlInjectionChallenge extends AssignmentEndpoint {
                 }
             } catch (SQLException e) {
                 attackResult = failed(this).output("Something went wrong").build();
-            }
+            } finally (Statement.close);
         }
         return attackResult;
     }
